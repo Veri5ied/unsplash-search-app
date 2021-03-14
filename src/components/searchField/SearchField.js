4ba5fdd29f6aa1ee2eda2d5029ce30ca7c8fa60f
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2021-03-14 12:08:59 +0100

    user search query

--- src/components/searchField/SearchField.js
+++ src/components/searchField/SearchField.js
@@ -3,17 +3,17 @@ import axios from "axios";
 import "./SearchField.css";
 
 function SeacrhField() {
-  const [image, setImage] = useState("");
+  const [user, setUser] = useState("");
   const [result, setResult] = useState([]);
 
   const accesskey = "AEbuzISbvALlvoN-X6Zoj-um36KXKln9Ys_oO5IygvE";
 
   const handleChange = (e) => {
-    setImage(e.target.value);
+    setUser(e.target.value);
   };
 
   const handleClick = () => {
-    const url = `https://api.unsplash.com/search/photos?page=1&query=${image}&client_id=${accesskey}`;
+    const url = `https://api.unsplash.com/search/users?page=4&query=${user}&client_id=${accesskey}`;
 
     axios
       .get(url)
@@ -28,14 +28,14 @@ function SeacrhField() {
   return (
     <div className="search__body">
       <div className="search__heading">
-        <h1>Unsplash Search App</h1>
+        <h1>Unsplash User Search App</h1>
       </div>
       <div className="search__input">
         <input
           onChange={handleChange}
           type="text"
           name="image"
-          value={image}
+          value={user}
           className="input__field"
           placeholder="Search for a user"
         />
@@ -45,11 +45,11 @@ function SeacrhField() {
       </div>
 
       <div className="search__result">
-        {result.map((image) => (
+        {result.map((user) => (
           <>
             <div className="card__list">
-              <img src={image.urls.thumb} alt="" />
-              <p className="card__image__name">Photo by {image.user.name}</p>
+              <img src={user.profile_image.large} alt="" />
+              <p className="card__image__name">Username: {user.username}</p>
             </div>
           </>
         ))}
